5724f4cbc7ae748743494345fc2e7214649d3531
J.T.McQuigg <J.T.McQuigg12@gmail.com>
2022-11-21 14:44:39 -0500

    Move to Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    java
+    `maven-publish`
+    idea
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven {
+        url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    }
+
+    maven {
+        url = uri("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+    }
+
+    maven {
+        url = uri("https://jitpack.io")
+    }
+
+    maven {
+        url = uri("https://repo.maven.apache.org/maven2/")
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains:annotations:23.0.0")
+    compileOnly("org.spigotmc:spigot-api:1.18.2-R0.1-SNAPSHOT")
+    compileOnly("me.clip:placeholderapi:2.11.2")
+    compileOnly("com.github.MilkBowl:VaultAPI:1.7")
+    compileOnly("io.netty:netty-all:4.1.85.Final")
+}
+
+group = "me.neznamy"
+version = "2.0.7"
+description = "TAB-Bridge"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
+    }
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "TAB-Bridge"
