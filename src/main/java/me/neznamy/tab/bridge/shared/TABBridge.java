b8c6da05ae26472c59ad889ad14e1bf24e5891ef
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2024-02-28 17:42:15 +0100

    5.0.3

--- src/main/java/me/neznamy/tab/bridge/shared/TABBridge.java
+++ src/main/java/me/neznamy/tab/bridge/shared/TABBridge.java
@@ -18,7 +18,7 @@ import java.util.concurrent.ScheduledExecutorService;
 public class TABBridge {
 
     public static final String CHANNEL_NAME = "tab:bridge-5";
-    public static final String PLUGIN_VERSION = "5.0.2";
+    public static final String PLUGIN_VERSION = "5.0.3";
     @Getter @Setter private static TABBridge instance;
 
     @Getter private final Platform platform;
