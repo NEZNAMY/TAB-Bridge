1134303f106fc4945d0343d9745bd81059f86ca2
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2024-04-25 22:12:49 +0200

    Remove duplicated code

--- src/main/java/me/neznamy/tab/bridge/bukkit/nms/NMSStorage.java
+++ src/main/java/me/neznamy/tab/bridge/bukkit/nms/NMSStorage.java
@@ -15,7 +15,6 @@ import org.jetbrains.annotations.Nullable;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -24,10 +23,10 @@ public class NMSStorage {
     //instance of this class
     @Nullable @Getter @Setter private static NMSStorage instance;
 
-    private final Class<?> Packet = getNMSClass("net.minecraft.network.protocol.Packet", "Packet");
-    private final Class<?> EntityPlayer = getNMSClass("net.minecraft.server.level.EntityPlayer", "EntityPlayer");
-    private final Class<?> NetworkManager = getNMSClass("net.minecraft.network.NetworkManager", "NetworkManager");
-    private final Class<?> PlayerConnection = getNMSClass("net.minecraft.server.network.PlayerConnection", "PlayerConnection");
+    private final Class<?> Packet = BukkitReflection.getClass("network.protocol.Packet", "Packet");
+    private final Class<?> EntityPlayer = BukkitReflection.getClass("server.level.EntityPlayer", "EntityPlayer");
+    private final Class<?> NetworkManager = BukkitReflection.getClass("network.NetworkManager", "NetworkManager");
+    private final Class<?> PlayerConnection = BukkitReflection.getClass ("server.network.PlayerConnection", "PlayerConnection");
     public final Field PLAYER_CONNECTION = ReflectionUtils.getFields(EntityPlayer, PlayerConnection).get(0);
     public final Field NETWORK_MANAGER = ReflectionUtils.getFields(BukkitBridge.is1_20_2Plus() ? PlayerConnection.getSuperclass() : PlayerConnection, NetworkManager).get(0);
     public final Field CHANNEL = ReflectionUtils.getFields(NetworkManager, Channel.class).get(0);
@@ -79,44 +78,5 @@ public class NMSStorage {
     private Method first(@NotNull List<Method> methods) throws NoSuchMethodException {
         if (methods.isEmpty()) throw new NoSuchMethodException("Json deserialize method not found");
         return methods.get(0);
-    }
-
-    /**
-     * Returns class with given potential names in same order
-     * @param names - possible class names
-     * @return class for specified name(s)
-     * @throws ClassNotFoundException if class does not exist
-     */
-    public Class<?> getNMSClass(String... names) throws ClassNotFoundException {
-        for (String name : names) {
-            try {
-                return BukkitBridge.getMinorVersion() >= 17 ? Class.forName(name) : getLegacyClass(name);
-            } catch (ClassNotFoundException e) {
-                //not the first class name in array
-            }
-        }
-        throw new ClassNotFoundException("No class found with possible names " + Arrays.toString(names));
-    }
-
-    /**
-     * Returns class from given name
-     * @param name - class name
-     * @return class from given name
-     * @throws ClassNotFoundException if class was not found
-     */
-    public Class<?> getLegacyClass(String name) throws ClassNotFoundException {
-        try {
-            return Class.forName("net.minecraft.server." + BukkitBridge.getServerPackage() + "." + name);
-        } catch (ClassNotFoundException | NullPointerException e) {
-            try {
-                //modded server?
-                Class<?> clazz = BukkitBridge.class.getClassLoader().loadClass("net.minecraft.server." + BukkitBridge.getServerPackage() + "." + name);
-                if (clazz != null) return clazz;
-                throw new ClassNotFoundException(name);
-            } catch (ClassNotFoundException | NullPointerException e1) {
-                //maybe fabric?
-                return Class.forName(name);
-            }
-        }
     }
 }
