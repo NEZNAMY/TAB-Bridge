8c2e0a9fcdb97f6b193c32d4215f7ab5ff6b94cc
NEZNAMY <n.e.z.n.a.m.y@azet.sk>
2025-04-09 17:07:28 +0200

    6.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 allprojects {
     group = "me.neznamy"
-    version = "6.0.1"
+    version = "6.0.2"
     description = "An addon to extend features with TAB on proxy"
 }
 
--- shared/src/main/java/me/neznamy/tab/bridge/shared/TABBridge.java
+++ shared/src/main/java/me/neznamy/tab/bridge/shared/TABBridge.java
@@ -17,7 +17,7 @@ import java.util.concurrent.ScheduledExecutorService;
 public class TABBridge {
 
     public static final String CHANNEL_NAME = "tab:bridge-6";
-    public static final String PLUGIN_VERSION = "6.0.1";
+    public static final String PLUGIN_VERSION = "6.0.2";
     @Getter @Setter private static TABBridge instance;
 
     @Getter private final Platform platform;
